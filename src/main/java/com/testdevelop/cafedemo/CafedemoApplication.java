33a3aa86dca375bf0fa4d964a7252580e764a22a
刘洋(Young) <y.liuv@ctrip.com>
2021-04-04 14:38:51 +0800

    create project

---
+++ src/main/java/com/testdevelop/cafedemo/CafedemoApplication.java
@@ -0,0 +1,13 @@
+package com.testdevelop.cafedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testdevelop/cafedemo/CafedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdevelop.cafedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
